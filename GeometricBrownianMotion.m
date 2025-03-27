65e7c8d5b047cea219be783ab0656e7db26212bc
Martin Westberg <121675207+MaWe96@users.noreply.github.com>
2025-03-27 14:29:10 +0100

    Commit matlab file

--- GeometricBrownianMotion.m
+++ GeometricBrownianMotion.m
@@ -85,16 +85,167 @@ E_PayoffEuP=0;
 E_PayoffAsC=0;
 E_PayoffAsP=0;
 for j=1:N
-S(1,1)=S0;
-for i=2:n
-S(i,1)=S(i-1,1)*exp((alpha)*dt+normrnd(0,1)*sqrt(dt)*Sigma);
+    S(1,1)=S0;
+    for i=2:n
+        S(i,1)=S(i-1,1)*exp((alpha)*dt+normrnd(0,1)*sqrt(dt)*Sigma);
+    end
+    St(j,1)=S(end,1);
+    E_PayoffEuC=E_PayoffEuC+max((St(j,1)-K),0)*1/N; % for EU Call Option
+    E_PayoffEuP=E_PayoffEuP+max(K-(St(j,1)),0)*1/N; % for EU Put Option
+    E_EuCdisc=E_PayoffEuC*exp(-mu); % Discounted to time 0 (Call Premium)
+    E_EuPdisc=E_PayoffEuP*exp(-mu); % Discounted to time 0 (Put Premium)
+    meanS(j,1)=mean(S); % Simplified with 1 sample data, interval size n.
+    E_PayoffAsC=E_PayoffAsC+ max((meanS(j,1)-K),0)*1/N; % for Asian Call Option
+    E_PayoffAsP= E_PayoffAsP+ max((K-meanS(j,1)),0)*1/N; % for Asian Put Option
 end
-St(j,1)=S(end,1);
-E_PayoffEuC=E_PayoffEuC+max((St(j,1)-K),0)*1/N; % for EU Call Option
-E_PayoffEuP=E_PayoffEuP+max(K-(St(j,1)),0)*1/N; % for EU Put Option
-E_EuCdisc=E_PayoffEuC*exp(-mu); % Discounted to time 0 (Call Premium)
-E_EuPdisc=E_PayoffEuP*exp(-mu); % Discounted to time 0 (Put Premium)
-meanS(j,1)=mean(S); % Simplified with 1 sample data, interval size n.
-E_PayoffAsC=E_PayoffAsC+ max((meanS(j,1)-K),0)*1/N; % for Asian Call Option
-E_PayoffAsP= E_PayoffAsP+ max((K-meanS(j,1)),0)*1/N; % for Asian Put Option
+figure(1)
+histogram(St); hold on
+histfit(St)
+format bank
+mean(St)
+% Example Output: mean(St)=2003.48, E_EuCdisc=252.97, E_EuPdisc=197.64
+% Example Output 2 (Asian Option): E_PayoffAsP= 130.2821, E_PayoffAsC= 139.9824
+title('Script (III): Simulated GBM with drift (5%) & volatility (30%)');
+xlabel('Price intervals');
+ylabel('Number of ST per interval');
+%figure(2)
+%plot(Time,S,'b'); % This command plots one of the many random walks.
+
+
+
+
+%% Script (III)s option price convergence plot (Run sections separately)
+T=1;
+N=linspace(100,1000000,10000); % number of sims
+for i=1:10000
+    St = S0 * exp((alpha) * T + Sigma*sqrt(T) * normrnd(0, 1, [1, N(1,i)]));
+    payoffEU_C(i,1) = exp(-mu * T) * mean(max(St - K, 0));
+    payoffEU_P(i,1) = exp(-mu * T) * mean(max(K - St, 0));
 end
+figure(1)
+plot(payoffEU_C); hold on
+plot(payoffEU_P); hold off
+title('Script (III)s option price convergence plot');
+xlabel('Simulations');
+ylabel('Option price');
+
+
+
+
+% Script (IV) simulates Geometric Brownian Motion at time 1 immediately
+% and determines expected option payoff and discounts it to time zero.
+% It concludes by plotting option price convergence.
+% The script can not plot GBM trajectories.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Section 1:
+clear all,
+clc
+%
+%
+S0 = 1909.6; % initial price
+T = 1; % time to maturity
+mu = 0.05; % short rate
+Sigma = 0.3; % volatility
+K = 1950; % strike price
+%N = 1000; % number of sims
+%N = 10000; % number of sims
+%N = 100000; % number of sims
+N = 1000000; % number of sims
+alpha = mu - (Sigma^2)/2;
+St = S0 * exp((alpha) * T + Sigma*sqrt(T) * normrnd(0, 1, [1, N]));
+E_PayoffEuC = exp(-mu * T) * mean(max(St - K, 0));
+E_PayoffEuP = exp(-mu * T) * mean(max(K - St, 0));
+E_EuCdisc = E_PayoffEuC*exp(-mu*T); % Discounted to time 0 (Call Premium)
+E_EuPdisc = E_PayoffEuP*exp(-mu*T); % Discounted to time 0 (Put Premium)
+mean(St);
+% Example Output: Call=252.6112, Put=198.1543, mean(St)=2007.2489
+
+
+
+
+%% Script (IV)s option price convergence plot (Run sections separately)
+N=linspace(100,1000000,10000); % number of sims
+for i=1:10000
+    St = S0 * exp((alpha) * T + Sigma*sqrt(T) * normrnd(0, 1, [1, N(1,i)]));
+    payoffEU_C(i,1) = exp(-mu * T) * mean(max(St - K, 0));
+    payoffEU_P(i,1) = exp(-mu * T) * mean(max(K - St, 0));
+end
+plot(payoffEU_C); hold on
+plot(payoffEU_P); hold off
+% log(N)
+title('Script (IV)s option price convergence plot');
+xlabel('Simulations');
+ylabel('Option price');
+
+
+
+
+% Script (V) simulates Geometric Brownian Motion with MATLAB’s built in
+% function over a year and plotts a histogram, which confirms Script (III).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all
+clc
+dt=1/252;
+t=252;
+S0=1909.6;
+mu=0.05;
+sigma=0.3;
+K=1950;
+N=1000000;
+GBM = gbm(mu, sigma, "StartState", S0);
+[x, y] = GBM.simulate(t, "DeltaTime", dt, "Ntrials", N);
+figure(1)
+histogram(x(end,:,:),50); hold on
+mean(x(end,:,:))
+%figure(2) % For plotting N simulated price movements
+%plot(y, squeeze(x))
+title('Script (V): Simulated GBM with drift (5%) & volatility (30%)');
+xlabel('Price intervals');
+ylabel('Number of ST per interval');
+% Example Output: mean(St)=2007.7
+
+
+
+
+% Script(VI) is a basic Black-Scholes Model valuation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mu=0.05;
+sigma=0.3;
+S0=1909.6;
+K=1950;
+[Call,Put] = blsprice(S0,K,mu,1,sigma)
+% Definitive Output: Call=252.8264, Put=198.1238 (serves as benchmark)
+% For S0=2831.2 from script (VII), and K=2900, Black Scholes results:
+% Call=370.7918, Put=298.1821
+
+
+
+
+% Script(VII) is a mixture of previous scripts. It simulates Geometric
+% Brownian Motion and calculates European Basket option payoff and
+% discounts time to zero. It then plots the option price convergence.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+S0M = [1909.6 3752.75]; % initial price for OMX and S&P
+weights = [.5; .5];
+S0 = S0M * weights; % in this case, S0=2831.2
+T = 1; % time to maturity
+mu = 0.05; % short rate
+sigma = 0.3; % volatility
+K = 2900; % strike price
+alpha = mu - (sigma^2)/2;
+N = 1000000;
+dN = 1000;
+E_PayoffEuC = zeros(N/dN,1);
+E_PayoffEuP = zeros(N/dN,1);
+for n = dN:dN:N
+    St = S0 * exp((mu - 0.5 * sigma^2) * T + sigma*sqrt(T) * normrnd(0, 1, [1,n]));
+    E_PayoffEuC(n/dN,1) = exp(-mu * T) * mean(max(St - K, 0));
+    E_PayoffEuP(n/dN,1) = exp(-mu * T) * mean(max(K - St, 0));
+end
+E_EuCdisc = E_PayoffEuC*exp(-mu*T); % Discounted to time 0 (Call Premium)
+E_EuPdisc = E_PayoffEuP*exp(-mu*T); % Discounted to time 0 (Put Premium)
+hold on
+plot(E_PayoffEuC)
+plot(E_PayoffEuP)
+hold off
+% Example Output: Call=370.7238, Put=298.1846
